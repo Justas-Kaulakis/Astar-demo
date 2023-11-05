9be87b372ae82236a183dff52a599bac2d06a250
Justas-Kaulakis <just.kaulakis@gmail.com>
2023-11-05 11:46:35 +0200

    setting up github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "https://github.com/Justas-Kaulakis/Astar-demo",
   build: {
     target: "esnext", //browsers can handle the latest ES features
   },
